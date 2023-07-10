0974a8b4d34032465c3f14a1391820ab19f92eaa
Rizal Anditama <rizalanditama2105@gmail.com>
2023-07-10 12:07:26 +0700

    rapihin dikit tampilan kartunya

--- app/Views/home.php
+++ app/Views/home.php
@@ -74,50 +74,50 @@
 </div>
 <br>
 
-<span id="popular" class="fw-bold mb-0 fs-2 grad-right p-3 my-3 rounded-5">Popular</span>
-<!-- <div id="popular" class="my-4 d-flex justify-content-center align-items-center overflow-x-auto"> -->
-<div class="my-4 row">
+<p id="popular" class="fw-bold mb-0 fs-2 grad-right p-3 rounded-5">Popular</p>
+<div class="my-4 row d-flex justify-content-center align-items-center">
     <?php foreach ($popular as $row) :
     ?>
-        <div class="col-lg-3 col-sm-4 col-6 d-flex align-items-stretch">
+        <div class="col-lg-3 col-sm-4 col-6 d-flex justify-content-center align-items-stretch">
             <div class="card border-0 mb-md-3">
-                <a href="<?= base_url('explore/' . $row['name']) ?>">
+                <a href="<?= base_url('explore/' . $row['name']) ?>" class="d-flex justify-content-center align-items-center">
                     <img loading="lazy" class="d-md-block d-none bd-placeholder-img card-img-top" src="<?= $row['poster_url'] ?>" alt="<?= $row['name'] ?>" style="width: 200px;">
-                    <img loading="lazy" class="d-xs-block d-md-none bd-placeholder-img card-img-top" src="<?= $row['poster_url'] ?>" alt="<?= $row['name'] ?>" style="width: 100px;">
+                    <img loading="lazy" class="d-sm-block d-md-none bd-placeholder-img card-img-top" src="<?= $row['poster_url'] ?>" alt="<?= $row['name'] ?>" style="width: 100px;">
                 </a>
-                <div class="card-body">
+                <div class="card-body text-center">
                     <span class="card-text"><?= $row['age_rating'] ?>+</span><br>
-                    <a href="<?= base_url('explore/' . $row['name']) ?>" class="vertical-truncate d-xs-block d-md-none card-title fw-bold text-decoration-none"><?= $row['name'] ?></a>
-                    <a href="<?= base_url('explore/' . $row['name']) ?>" class=" d-md-block d-none card-title h3 text-decoration-none"><?= $row['name'] ?></a>
+                    <a href="<?= base_url('explore/' . $row['name']) ?>" class="vertical-truncate d-sm-block d-md-none card-title fw-bold text-decoration-none"><?= $row['name'] ?></a>
+                    <a href="<?= base_url('explore/' . $row['name']) ?>" class="d-md-block d-none card-title h3 text-decoration-none"><?= $row['name'] ?></a>
                 </div>
             </div>
         </div>
     <?php endforeach;
     unset($row);
     ?>
+</div>
 
-    <span id="now" class="fw-bold mb-0 fs-2 grad-right p-3 mt-5 mb-3 rounded-5">Now Playing</span>
-    <div class="my-4 row d-flex justify-content-center align-items-center">
-        <?php foreach ($data as $row) :
-        ?>
-            <div class="col-lg-3 col-sm-4 col-6 d-flex align-items-stretch">
-                <div class="card border-0 mb-md-3">
-                    <a href="<?= base_url('explore/' . $row['name']) ?>">
-                        <img loading="lazy" class="d-md-block d-none bd-placeholder-img card-img-top" src="<?= $row['poster_url'] ?>" alt="<?= $row['name'] ?>" style="width: 200px;">
-                        <img loading="lazy" class="d-xs-block d-md-none bd-placeholder-img card-img-top" src="<?= $row['poster_url'] ?>" alt="<?= $row['name'] ?>" style="width: 100px;">
-                    </a>
-                    <div class="card-body">
-                        <span class="card-text"><?= $row['age_rating'] ?>+</span><br>
-                        <a href="<?= base_url('explore/' . $row['name']) ?>" class="vertical-truncate d-xs-block d-md-none card-title fw-bold text-decoration-none"><?= $row['name'] ?></a>
-                        <a href="<?= base_url('explore/' . $row['name']) ?>" class=" d-md-block d-none card-title h3 text-decoration-none"><?= $row['name'] ?></a>
-                    </div>
+<p id="now" class="fw-bold mb-0 fs-2 grad-right p-3 rounded-5">Now Playing</p>
+<div class="my-4 row d-flex justify-content-center align-items-center">
+    <?php foreach ($data as $row) :
+    ?>
+        <div class="col-lg-3 col-sm-4 col-6 d-flex justify-content-center align-items-stretch">
+            <div class="card border-0 mb-md-3">
+                <a href="<?= base_url('explore/' . $row['name']) ?>" class="d-flex justify-content-center align-items-center">
+                    <img loading="lazy" class="d-md-block d-none bd-placeholder-img card-img-top" src="<?= $row['poster_url'] ?>" alt="<?= $row['name'] ?>" style="width: 200px;">
+                    <img loading="lazy" class="d-sm-block d-md-none bd-placeholder-img card-img-top" src="<?= $row['poster_url'] ?>" alt="<?= $row['name'] ?>" style="width: 100px;">
+                </a>
+                <div class="card-body text-center">
+                    <span class="card-text"><?= $row['age_rating'] ?>+</span><br>
+                    <a href="<?= base_url('explore/' . $row['name']) ?>" class="vertical-truncate d-sm-block d-md-none card-title fw-bold text-decoration-none"><?= $row['name'] ?></a>
+                    <a href="<?= base_url('explore/' . $row['name']) ?>" class="d-md-block d-none card-title h3 text-decoration-none"><?= $row['name'] ?></a>
                 </div>
             </div>
-        <?php endforeach;
-        unset($row);
-        ?>
+        </div>
+    <?php endforeach;
+    unset($row);
+    ?>
+</div>
 
 
 
-
-        <?= $this->endSection(); ?>
+<?= $this->endSection(); ?>
