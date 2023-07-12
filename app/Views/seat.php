177412eb67b7cc73a08f5cd6b47d893c55fa66da
Rizal Anditama <rizalanditama2105@gmail.com>
2023-07-12 21:55:10 +0700

    tried making the seat booker responsive (failed)

--- app/Views/seat.php
+++ app/Views/seat.php
@@ -14,18 +14,19 @@ if (session()->get('selected')) {
     <div class="mb-3 d-flex justify-content-center align-items-center">
         <h3>Book your seats (<?= count($selected) ?> out of <?= $seat ?? 0 ?>)</h3>
     </div>
-    <div class="d-flex justify-content-center align-items-center">
-        <div class="row text-center d-flex justify-content-center align-items-center w-50">
-            <!-- (.col-2>btn.btn.btn-success.m-1{$})*60^.d-flex.justify-content-center>(.col-2>btn.btn.btn-success.m-1{$})*4 -->
-            <?php for ($i = 0; $i < 64; $i++) : ?>
-                <div class="col-1 m-1">
-                    <form action="seat/<?= $i + 1 ?>" method="post">
-                        <button type="submit" class="btn <?= in_array($i + 1, $selected) ? 'btn-success' : (in_array($i + 1, $booked) ? 'btn-danger' : 'btn-disabled border-success'); ?>" style="min-width: 45px;" <?= (in_array($i + 1, $selected) || in_array($i + 1, $booked)) ? 'disabled' : ''; ?> <?= (!$seat) ? 'disabled' : ''; ?>><?= $i + 1  ?></button>
-                    </form>
-                </div>
-            <?php endfor; ?>
+</div>
+<div class="d-flex justify-content-center align-items-center">
+    <div class="row d-flex justify-content-center align-items-center seat">
+        <!-- (.col-2>btn.btn.btn-success.m-1{$})*60^.d-flex.justify-content-center>(.col-2>btn.btn.btn-success.m-1{$})*4 -->
+        <?php for ($i = 0; $i < 64; $i++) : ?>
+            <div class="col-1 m-1">
+                <form action="seat/<?= $i + 1 ?>" method="post">
+                    <button type="submit" class="btn <?= in_array($i + 1, $selected) ? 'btn-success' : (in_array($i + 1, $booked) ? 'btn-danger' : 'btn-disabled border-success'); ?>" style="min-width: 45px;" <?= (in_array($i + 1, $selected) || in_array($i + 1, $booked)) ? 'disabled' : ''; ?> <?= (!$seat) ? 'disabled' : ''; ?>><?= $i + 1  ?></button>
+                </form>
+            </div>
+        <?php endfor; ?>
 
-            <!-- <div class="d-flex justify-content-center">
+        <!-- <div class="d-flex justify-content-center">
                 <?php for ($i = 60; $i < 64; $i++) : ?>
                     <div class="col-1 m-1">
                         <form action="seat/<?= $i + 1 ?>" method="post">
@@ -34,7 +35,6 @@ if (session()->get('selected')) {
                     </div>
                 <?php endfor; ?>
             </div> -->
-        </div>
     </div>
 </div>
 
